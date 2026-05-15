0c266835990e58d95a53e31057879091c2444573
Matteo Calcagni <matteo.calcagni03@gmail.com>
2026-05-15 22:46:22 +0200

    Selected the needed files for the Panoptic FPN

---
+++ Panoptic_model/backbone.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from torch import nn
+
+from containers import SequentialMultiInputMultiOutput, SequentialMultiOutput
+from layers import Sum
+
+
+class ResNetFeatureMapsExtractor(nn.Module):
+    def __init__(self, model: nn.Module, mode: Optional[str] = None):
+        super().__init__()
+        self.mode = mode
+        # yapf: disable
+        stem = nn.Sequential(
+            model.conv1,
+            model.bn1,
+            model.relu,
+            model.maxpool
+        )
+        layers = [
+            model.layer1,
+            model.layer2,
+            model.layer3,
+            model.layer4,
+        ]
+        if mode == 'fusion':
+            # allow each layer to take in multiple inputs - summming them
+            # before passing them through. This allows each layer to take in
+            # in feature maps from multiple backbones.
+            multi_input_layers = [
+                nn.Sequential(Sum(), layer) for layer in layers
+            ]
+            self.m = SequentialMultiInputMultiOutput(
+                stem,
+                *multi_input_layers,
+                Sum()
+            )
+        else:
+            self.m = SequentialMultiOutput(stem, *layers)
+        # yapf: enable
+
+    def forward(self, x):
+        if self.mode != 'fusion':
+            return self.m(x)
+        x, in_feats = x
+        return self.m((x, *in_feats))
+
+
+
---
+++ Panoptic_model/factory.py
@@ -0,0 +1,85 @@
+from typing import Tuple
+
+import torch
+from torch import nn
+import torchvision as tv
+
+from layers import Interpolate
+from fpn import PanopticFPN
+from utils import _get_shapes
+from backbone import ResNetFeatureMapsExtractor
+
+
+
+
+def make_fpn_resnet(name: str = 'resnet18',
+                    fpn_type: str = 'fpn',
+                    out_size: Tuple[int, int] = (224, 224),
+                    fpn_channels: int = 256,
+                    num_classes: int = 1000,
+                    pretrained: bool = True,
+                    in_channels: int = 3) -> nn.Module:
+    """Create an FPN model with a ResNet backbone.
+
+    If `in_channels > 3`, uses the fusion technique described in the paper,
+    *FuseNet*, by Hazirbas et al.
+    (https://vision.in.tum.de/_media/spezial/bib/hazirbasma2016fusenet.pdf)
+    that adds a parallel resnet backbone for the new channels. All the
+    pretrained weights are retained.
+
+    Args:
+        name (str, optional): Name of the resnet backbone. Only those available
+            in torchvision are supported. Defaults to 'resnet18'.
+        fpn_type (str, optional): Type of FPN. 'fpn' | 'panoptic' | 'panet'.
+            Defaults to 'fpn'.
+        out_size (Tuple[int, int], optional): Size of segmentation output.
+            Defaults to (224, 224).
+        fpn_channels (int, optional): Number of hidden channels to use in the
+            FPN. Defaults to 256.
+        num_classes (int, optional): Number of classes for which to make
+            predictions. Determines the channel width of the output.
+            Defaults to 1000.
+        pretrained (bool, optional): Whether to use pretrained backbone.
+            Defaults to True.
+        in_channels (int, optional): Channel width of the input. If less than
+            3, conv1 is replaced with a smaller one. If greater than 3, a
+            FuseNet-style architecture is used to incorporate the new channels.
+            In both cases, pretrained weights are retained. Defaults to 3.
+
+    Raises:
+        NotImplementedError: On unknown fpn_style.
+
+    Returns:
+        nn.Module: the FPN model
+    """
+
+    # Keep these checks
+
+    assert in_channels > 0
+    assert num_classes > 0
+    assert out_size[0] > 0 and out_size[1] > 0
+
+    resnet = tv.models.resnet.__dict__[name](pretrained=pretrained)
+    if in_channels == 3:
+        backbone = ResNetFeatureMapsExtractor(resnet)
+
+    
+    feat_shapes = _get_shapes(backbone, channels=in_channels, size=out_size)
+    
+    if fpn_type == 'panoptic':
+        fpn = PanopticFPN(
+            feat_shapes,
+            hidden_channels=fpn_channels,
+            out_channels=num_classes)
+    
+    else:
+        raise NotImplementedError()
+ 
+    # BUILD THE MODEL
+    # yapf: disable
+    model = nn.Sequential(
+        backbone,
+        fpn,
+        Interpolate(size=out_size))
+    # yapf: enable
+    return model
